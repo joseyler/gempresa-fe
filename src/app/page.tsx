11861425b01f933007d7b765044627792f1118d9
Juan1Suarez <juansuarez60249@gmail.com>
2024-10-10 17:20:15 -0300

    Google chart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,88 +1,87 @@
 "use client"
-import React, { useState } from 'react';
-import { Chart, registerables } from 'chart.js';
-import { Line } from "react-chartjs-2";
+import React from 'react';
+import { Chart } from "react-google-charts";
 import styles from "./page.module.css";
 
 export default function Home() {
 
-  Chart.register(...registerables);
+  //LINE CHART
+  const dataLine = [
+    ["Años", "Amazon", "Mercado libre", "Ebay"],
+    ["2014", 1050, 560, 1000],
+    ["2015", 860, 1090, 800],
+    ["2016", 1070, 740, 600],
+    ["2017", 1110, 660, 800],
+    ["2018", 1040, 620, 1000],
+    ["2019", 1020, 710, 100],
+    ["2020", 920, 1220, 1000],
+    ["2021", 1120, 850, 700],
+    ["2022", 1080, 810, 892],
+    ["2023", 1120, 820, 1000],
+  ];
 
-
-  const scales = {
-    y: {
-      ticks: {
-        beginAtZero: true,
-        callback(value: number) {
-          const formattedValue = (value).toString().replace('-', '');
-          if (value >= 0) {
-            return `$${formattedValue}`;
-          }
-          return `-$${formattedValue}`;
-        },
-      },
+  const optionsLine = {
+    chart: {
+      title: "E-Stores",
+      subtitle: "Tiendas con envios, por millon de dolares",
     },
-    x: {
-      ticks: {
-        font: {
-          size: 10.5,
-        },
-      },
-    },
+    width: 950,
+    height: 500,
+    colors: ["#3498db", "#f4d03f", "#2ecc71"],
+    curveType: "function",
+    lineWidth: 4,
+    pointSize: 7,
+    legend: { position: "bottom" },
   };
 
-  const [configChart] = useState({
-    plugins: {
-      maintainAspectRatio: false,
-      title: "Gempresa Stock Values",
-      legend: {
-        position: 'top',
-        align: 'center',
-      },
-    },
-    scales,
-    responsive: true,
-    maintainAspectRatio: false,
-  });
+  //PIE CHART
 
-  const dataChart = () => {
-    const dataChart = {
-      labels: ['2024-04-01', '2024-04-02', '2024-04-03'
-        ,'2024-04-04', '2024-04-05', '2024-04-06'
-        ,'2024-04-07', '2024-04-08', '2024-04-09'
-        ,'2024-04-10', '2024-04-11', '2024-04-12'
-        ,'2024-04-13', '2024-04-14', '2024-04-15'
-      ],
-      datasets: [
-        {
-          label: 'Amazon',
-          data: [18, 20, 22, 18, 20, 22, 23, 23, 24, 23, 23, 24, 25, 28, 32],
-          hoverOffset: 4,
-          backgroundColor: 'yellow',
-          borderColor: 'yellow',
-        }, {
-          label: 'Tesla',
-          data: [33, 32, 34, 33, 32, 34, 33, 32, 34, 33, 32, 34, 33, 32, 34],
-          hoverOffset: 4,
-          backgroundColor: 'blue',
-          borderColor: 'blue',
-        }
-      ],
-    }
-    return dataChart;
+  const dataPie = [
+    ["Companias", " Valor basado en porcentaje por millon de dolares"],
+    ["Nvidia", 22,],
+    ["Intel", 7],
+    ["AMD", 13],
+    ["Asus", 5],
+    ["MSI", 6],
+  ];
+
+  const optionsPie = {
+    title: "Companias de tecnologia",
+    width: 950,
+    height: 500,
+    colors: ["#2ecc71", "#3498db", "#e74c3c", "#8e44ad", "#e67e22"],
   };
 
+
+  //COLUMN CHART
+
+ const dataColumn = [
+    ["Elementos", "Densidad", { role: "style" }],
+    ["Cobre", 8.94, "#b87333"],
+    ["Plata", 10.49, "silver"],
+    ["Oro", 19.3, "gold"],
+    ["Platino", 21.45, "color: #e5e4e2"],
+  ];
 
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <div style={{ backgroundColor: 'white' }}>
-          <Line
-            options={configChart}
-            data={dataChart()}
-            width="800px"
-          />
-        </div>
+        <Chart
+          chartType="Line"
+          data={dataLine}
+          options={optionsLine}
+        />
+        <br></br>
+
+<h1>{dataPie[0][0]},{dataPie[0][1]} </h1>
+        <Chart
+          chartType="PieChart"
+          data={dataPie}
+          options={optionsPie}
+        />
+        <br></br>
+
+         <Chart chartType="ColumnChart" data={dataColumn} />
       </main >
     </div >
   );
